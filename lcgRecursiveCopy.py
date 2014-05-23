f044aaa0e14737c25d6f93608df447d7e1dd2c99
Pietro Vischia <pietro.vischia@gmail.com>
2014-05-23 14:23:13 +0100

    Full fledged version, with paths from command line

--- lcgRecursiveCopy.py
+++ lcgRecursiveCopy.py
@@ -1,6 +1,8 @@
 from os import walk
 from os.path import splitext, join
 
+import os,sys
+
 """
 lcgRecursiveCopy.py
 -------------------
@@ -11,10 +13,32 @@ Author: Pietro Vischia, pietro.vischia@gmail.com
 """
 
 
-def selectFiles(root, files):
+# Python 2.7: from argparse import ArgumentParser
+from optparse import OptionParser
+# Python 2.7: parser = OptionParser(description='This script copies recursively a local directory to a Tier2/3 (for now, only T2_PT_NCG_Lisbon)')
+parser = OptionParser(description='This script copies recursively a local directory to a Tier2/3 (for now, only T2_PT_NCG_Lisbon)')
+
+parser.add_option("-u", "--username", dest="username", help="username (will be used for creating the final path in the storage area)")
+parser.add_option("-i", "--input",    dest="input",    help="source directory (absolute local path)")
+parser.add_option("-o", "--output",   dest="output",   help="destination directory: it is assumed to be specified as a relative path starting from AFTER /cmst3/store/user/")
+
+# Python 2.7: args = parser.parse_args()
+(options, args) = parser.parse_args()
+if not options.username:
+    parser.print_help()
+    parser.error("No username provided")
+if not options.input:
+    parser.print_help()
+    parser.error("No input directory provided")
+if not options.output:
+    parser.print_help()
+    parser.error("No output directory provided")
+
+
+def selectFiles(root, files, path):
 
     selectedFiles = []
-
+    selectedFilesWithPath = []
     for file in files:
         #do concatenation here to get full path 
         fullPath = join(root, file)
@@ -24,42 +48,59 @@ def selectFiles(root, files):
         #        ext = splitext(file)[1]
         #        if ext == ".py":
         #            selected_files.append(full_path)
+        selectedFilesWithPath.append(fullPath)
+        print fullPath
+        print "PATH IS " + path
+        fullPath = fullPath.replace(path,'') 
+        print fullPath
         selectedFiles.append(fullPath)
-
-    return selectedFiles
+    return {'files': selectedFiles, 'filesWithPath': selectedFilesWithPath }
 
 def buildRecursiveDirTree(path):
     """
     path    -    where to begin folder scan
     """
     selectedFiles = []
-
+    selectedFilesWithPath = []
     for root, dirs, files in walk(path):
-        selectedFiles += selectFiles(root, files)
+        result = selectFiles(root,files,path)
+        selectedFiles += result['files']
+        selectedFilesWithPath += result['filesWithPath']
 
-    return selectedFiles
+    return selectedFiles, selectedFilesWithPath
 
-def lcgCopyDirTree(files):
+def lcgCopyDirTree(username, relDestPath, files, filesWithPath):
     """
     Invoke lcg-cp to copy files to the desired Tier2
     """
     print "Now copying"
-    username="vischia"
-    relDestPath="storage/test"
     tier="srm://srm01.ncg.ingrid.pt:8444/srm/managerv2?SFN=/cmst3/store/user/"
     destPath=tier+username+"/"+relDestPath
 
-    for f in files:
-        cmd='lcg-cp --verbose -b -D srmv2 file://'+f+' "'+destPath+'" >> LOG'+' &'
-        print cmd
-        #        os.system('lcg-cp --verbose -b -D srmv2 file://"'+f+' "'+destPath+'" >> LOG'+' &') 
+    for i in range(len(files)):
+        cmd='lcg-cp --verbose -b -D srmv2 file://'+filesWithPath[i]+' "'+destPath+'/'+files[i]+'" >> LOG'+' &'
+        os.system(cmd) 
 
 
 # Main
+if len(sys.argv) == 0:
+    print "Usage: " + sys.argv[0] + " /path/to/input/directory " + " relative/path/to/Destination"
+    print "Warning: destination is assumed to be specified from after /cmst3/store/user/"
+    
+if sys.argv[1] == "--help":
+    print "Usage: " + sys.argv[0] + " /path/to/input/directory " + " relative/path/to/Destination"
+    print "Warning: destination is assumed to be specified from after /cmst3/store/user/"
 
-myDir="/var/log/"
-print "List directory " + myDir
-print "======================================================="
-listing=buildRecursiveDirTree(myDir)
-print listing
-lcgCopyDirTree(listing)
+# Python 2.7
+#inDir =args.input
+#outDir=args.output
+
+print "Username: " + options.username
+print "Source directory: " + options.input
+print "Destination directory: " + options.output
+
+listFiles = []
+listFilesWithPath = []
+listFiles, listFilesWithPath=buildRecursiveDirTree(options.input)
+print listFilesWithPath
+lcgCopyDirTree(options.username,options.output,listFiles,listFilesWithPath)
